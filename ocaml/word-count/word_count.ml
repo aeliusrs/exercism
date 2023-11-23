6c703b2d84b952ef7a657ff909a1902b1653be4b
asrs <asrs.contact@gmail.com>
2023-11-23 17:08:14 +0800

    ocaml: optimized word count :)

--- ocaml/word-count/word_count.ml
+++ ocaml/word-count/word_count.ml
@@ -1,28 +1,23 @@
 open Base
 
+let is_valid c = Char.is_alphanum c || Char.equal c ' ' || Char.equal c '\''
+
+let clean w = w
+    |> String.lowercase
+    |> String.chop_prefix_if_exists ~prefix:"'"
+    |> String.chop_suffix_if_exists ~suffix:"'"
+
 let preprocess txt =
-  let is_valid c =
-    Char.is_alphanum c || Char.equal c ' ' || Char.equal c '\''
-  in
   txt
-  |> String.lowercase
   |> String.map ~f:(fun c -> if is_valid c then c else ' ')
   |> String.split ~on:' '
-  |> List.filter ~f:(fun s -> not(String.is_empty s))
-  |> List.map ~f:(fun w ->
-      w
-      |> String.chop_prefix_if_exists ~prefix:"'"
-      |> String.chop_suffix_if_exists ~suffix:"'"
+  |> List.filter_map ~f:(fun w ->
+      match String.is_empty w with
+      | true -> None
+      | false -> Some (clean w , 1)
   )
 
 let word_count txt =
-  preprocess txt
-  |> List.fold
-    ~init:(Map.empty (module String))
-    ~f:(fun acc word ->
-      Map.update acc word ~f:(function
-        | Some count -> count + 1
-        | None -> 1
-      )
-    )
-
+  txt
+  |> preprocess
+  |> Map.of_alist_fold (module String) ~init:0 ~f:(+)
