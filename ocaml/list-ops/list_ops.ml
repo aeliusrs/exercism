032f0f90e1af0c326c6c2776f8bc21d04b545e81
asrs <asrs.contact@gmail.com>
2023-11-23 12:46:43 +0800

    ocaml: finish list_ops

---
+++ ocaml/list-ops/list_ops.ml
@@ -0,0 +1,25 @@
+let rec fold ~init ~f = function
+  | [] -> init
+  | hd :: tl -> fold ~init:(f init hd) ~f tl [@tail]
+
+let reverse t =
+  fold ~init:[] ~f:(fun acc e -> e :: acc) t
+
+let length t =
+  fold ~init:0 ~f:(fun acc _ -> succ acc) t
+
+let fold_right ~init ~f t =
+  reverse t |> fold ~init ~f
+
+let map ~f t =
+  fold_right ~init:[] ~f:(fun acc e -> (f e) :: acc) t
+
+let filter ~f t =
+  fold_right ~init:[] 
+  ~f:(fun acc e -> if f e then e :: acc else acc) t
+
+let append t t' =
+  fold_right ~init:t' ~f:(fun acc e -> e :: acc) t
+
+let concat t =
+  fold_right ~init:[] ~f:(fun acc e -> append e acc) t
