2cb205a679c3485a8436c8b05c590472dfb2b6c9
asrs <asrs.contact@gmail.com>
2023-11-23 17:34:25 +0800

    ocaml: iterate over word_count to discover new function such as Map.of_alist_fold, String.strip ~drop:(f) ...etc finally push a optimized but readable solution

--- ocaml/word-count/word_count.ml
+++ ocaml/word-count/word_count.ml
@@ -2,12 +2,9 @@ open Base
 
 let is_valid c = Char.is_alphanum c || Char.equal c ' ' || Char.equal c '\''
 
-let clean w = w
-    |> String.lowercase
-    |> String.chop_prefix_if_exists ~prefix:"'"
-    |> String.chop_suffix_if_exists ~suffix:"'"
+let clean w = w |> String.lowercase |> String.strip ~drop:(Char.equal '\'')
 
-let preprocess txt =
+let word_count txt =
   txt
   |> String.map ~f:(fun c -> if is_valid c then c else ' ')
   |> String.split ~on:' '
@@ -16,8 +13,4 @@ let preprocess txt =
       | true -> None
       | false -> Some (clean w , 1)
   )
-
-let word_count txt =
-  txt
-  |> preprocess
   |> Map.of_alist_fold (module String) ~init:0 ~f:(+)
