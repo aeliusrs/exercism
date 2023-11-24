9e56ad5deac4d9586041c16c0ae8346d0fcb5414
aeliusrs <asrs.contact+dev@gmail.com>
2023-11-24 08:33:59 +0000

    Ocaml: Improve phone number

--- ocaml/phone-number/phone_number.ml
+++ ocaml/phone-number/phone_number.ml
@@ -1,4 +1,5 @@
 open Base
+open Result
 
 let is_valid acc c = match c with
   | '0'..'9' -> acc
@@ -6,32 +7,19 @@ let is_valid acc c = match c with
   | 'a'..'z' | 'A'..'Z' -> Error "letters not permitted"
   | _ -> Error "punctuations not permitted"
 
-let check_length str =
-  match (String.is_prefix ~prefix:"1" str, String.length str) with
-  | true, 10 -> Ok str
-  | true, 11 -> Ok (String.chop_prefix_exn ~prefix:"1" str)
-  | false, 10 -> Ok str
-  | false, 11 -> Error "11 digits must start with 1"
-  | _, n when n > 11 -> Error "more than 11 digits"
-  | _, _ -> Error "incorrect number of digits"
-
-let check_code str =
-  match (String.get str 0, String.get str 3) with
-  | '0', _ -> Error "area code cannot start with zero"
-  | '1', _ -> Error "area code cannot start with one"
-  | _, '0' -> Error "exchange code cannot start with zero"
-  | _, '1' -> Error "exchange code cannot start with one"
-  | _ -> Ok str
-
 let number str =
-  str
-  |> String.fold ~init:(Ok str) ~f:(fun acc c -> is_valid acc c)
-  |> function (* clean the number *)
-    | Error m -> Error m
-    | Ok s -> Ok (String.filter ~f:Char.is_digit s)
-  |> function (* check length and remove the country code *)
-    | Error m -> Error m
-    | Ok s -> check_length s
-  |> function (* check the validity of area and exchange code *)
-    | Error m -> Error m
-    | Ok s -> check_code s
+  String.fold ~init:(Ok str) ~f:is_valid str
+  >>= (fun s -> Ok (String.filter ~f:Char.is_digit s))
+  >>= (fun s -> match (String.is_prefix ~prefix:"1" s, String.length s) with
+      | true, 10 -> Ok s
+      | true, 11 -> Ok (String.chop_prefix_exn ~prefix:"1" s)
+      | false, 10 -> Ok s
+      | false, 11 -> Error "11 digits must start with 1"
+      | _, n when n > 11 -> Error "more than 11 digits"
+      | _, _ -> Error "incorrect number of digits")
+  >>= (fun s -> match (String.get s 0, String.get s 3) with
+      | '0', _ -> Error "area code cannot start with zero"
+      | '1', _ -> Error "area code cannot start with one"
+      | _, '0' -> Error "exchange code cannot start with zero"
+      | _, '1' -> Error "exchange code cannot start with one"
+      | _ -> Ok s)
