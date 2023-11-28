afaa8ddafcb9b431111be09e9511bac13c5556d6
aeliusrs <asrs.contact+dev@gmail.com>
2023-11-28 14:12:39 +0800

    asm: Pushing finallly two_fer

--- x86-64-assembly/two-fer/two_fer.asm
+++ x86-64-assembly/two-fer/two_fer.asm
@@ -1,60 +1,49 @@
-default rel
-
 ; Remember to comment out TEST_IGNORE() in two_fer_test.c
 ; Then run tests with 'make'
 
+; PLACEMENT OF VARIABLE IS IMPORTANT, especially for equ instruction
 section .rodata
-  prefix db "One for "
   noarg db "you"
-  suffix db ", one for me.", 0x0
-
   noarg_len equ $-noarg
+
+  prefix db "One for "
   prefix_len equ $-prefix
+
+  suffix db ", one for me.", 0x0
   suffix_len equ $-suffix
 
-global two_fer
 section .text
+global two_fer
 two_fer:
-    mov r8, rdi
+	mov r8, rdi ; name in rax
+	mov rdi, rsi ; buffer to rdi
 
-    jmp .print_prefix
+print_prefix:
+	lea rsi, [rel prefix]
+	mov rcx, prefix_len
+	rep movsb
 
-    cmp r8, 0
-    jz .print_noarg
-;    jmp .print_arg
+	cmp r8, 0; check given name
+	jz .print_noarg
 
-    jmp .print_suffix
-    ret
+	.print_arg:
+		mov rsi, r8
+		.loop:
+			cmp byte [rsi], 0
+			je .print_suffix
+			movsb
+			jmp .loop
 
-    .print_prefix:
-    mov rdi, rsi
-    lea rsi, [rel prefix]
-    mov rcx, prefix_len
-    repe movsb
-    ret
+	.print_noarg:
+		lea rsi, [rel noarg]
+		mov rcx, noarg_len
+		rep movsb
 
-    .print_noarg:
-    mov rdi, rsi
-    lea rsi, [rel noarg]
-    mov rcx, noarg_len
-    repe movsb
-    ret
-
-    .print_arg:
-    mov rdi, rsi
-    lea rsi, [rel r8]
-    mov rcx, 10
-    repe movsb
-    ret
-
-
-    .print_suffix:
-    mov rdi, rsi
-    lea rsi, [rel suffix]
-    mov rcx, suffix_len
-    repe movsb
-    ret
-
+	.print_suffix:
+		lea rsi, [rel suffix]
+		mov rcx, suffix_len
+		rep movsb
+		ret
 
 
 %ifidn __OUTPUT_FORMAT__,elf64
