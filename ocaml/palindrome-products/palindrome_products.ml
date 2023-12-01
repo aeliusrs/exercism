b3e64b1bf1009c445259ad5013b372e1abee7463
aeliusrs <asrs.contact+dev@gmail.com>
2023-12-01 10:07:40 +0800

    ocaml: Better iteration, and performance for palindrome_products

--- ocaml/palindrome-products/palindrome_products.ml
+++ ocaml/palindrome-products/palindrome_products.ml
@@ -1,5 +1,4 @@
 open Base
-open Sequence
 
 type palindrome_products = { value : int option; factors : (int * int) list; }
 
@@ -7,39 +6,32 @@ let is_palindrome number =
   let n = Int.to_string number in
   String.(rev n = n)
 
-let get_palindrome min max =
-  let range = List.range ~stop:`inclusive min max in
-  List.concat_map range ~f:(fun x ->
-     Sequence.unfold_step ~init:range ~f:(function
-       | [] -> Step.Done
-       | hd :: tl -> let f = x * hd in
-           match (x < hd || x = hd) && is_palindrome f with
-           | true   -> Step.Yield { value = (f, (x, hd)); state = tl }
-           | false  -> Step.Skip { state = tl })
-     |> Sequence.to_list)
-  |> List.sort ~compare:(fun (f, _) (f', _) -> Int.compare f f')
-  |> List.group ~break:(fun (f, _) (f', _) -> f <> f')
-  |> List.map ~f:(fun sublst ->
-      List.fold sublst ~init:(0, []) ~f:(fun (_, acc) (f, p) -> (f, acc @ [p])))
+let get_palindrome ~min ~max ~finish =
+  let lst = Sequence.range ~stop:`inclusive min max in
+  Sequence.concat_mapi lst ~f:(fun i x -> (* loop of loop to generate tuple *)
+    Sequence.drop lst i |> Sequence.map ~f:(fun y -> (x,y))) 
+  |> Sequence.fold ~init:(Map.empty (module Int)) ~f:(fun acc (a, b) ->
+      let f = a * b in
+      if a <= b && is_palindrome f then
+        Map.add_multi acc ~key:f ~data:(a,b)
+      else acc)
+  |> Map.to_alist (* retrieve the palindrome map to list *)
+  |> finish (* pass the finish function to get element *)
+  |> function (* format the element *)
+    | Some (pal, lst) -> Ok {value = Some pal; factors = lst}
+    | None -> Ok {value=None; factors=[]}
 
 let smallest ~min ~max =
-  if min > max then
-    Error "min must be <= max"
-  else
-    match get_palindrome min max |> Fn.flip List.nth 0 with
-    | Some (f, lst) -> Ok {value = Some f; factors = lst}
-    | None -> Ok {value=None; factors=[]}
+  match min <= max with
+  | true -> get_palindrome ~min:min ~max:max ~finish:List.hd
+  | false -> Error "min must be <= max"
 
 let largest ~min ~max =
-  if min > max then
-    Error "min must be <= max"
-  else
-    match get_palindrome min max |> List.last with
-    | Some (f, lst) -> Ok {value = Some f; factors = lst}
-    | None -> Ok {value=None; factors=[]}
+  match min <= max with
+  | true -> get_palindrome ~min:min ~max:max ~finish:List.last
+  | false -> Error "min must be <= max"
 
-let show_palindrome_products p =
-  match p.value with
+let show_palindrome_products p = match p.value with
   | Some x ->
       let lst = List.hd_exn p.factors in
       Printf.sprintf "{%d; [%d, %d]}" x (fst lst) (snd lst)
