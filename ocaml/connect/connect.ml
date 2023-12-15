c96c55e6f33def6d5f7de2719a71202adc09c784
aeliusrs <asrs.contact+dev@gmail.com>
2023-12-15 18:14:13 +0800

    ocaml: connect, light refactoring to match better logic using List.for_all for bool list checking

--- ocaml/connect/connect.ml
+++ ocaml/connect/connect.ml
@@ -13,11 +13,9 @@ let retrieve board y x = (* get a cell in the board *)
 let get_ps board player y = (* get all position of player in certain columns *)
   board
   |> List.map ~f:(fun l -> List.nth l y)
-  |> List.foldi ~init:[] ~f:(fun index acc e ->
-      match e with
+  |> List.foldi ~init:[] ~f:(fun index acc e -> match e with
       | None -> acc
       | Some e' -> if Char.equal e' player then (index, y) :: acc else acc)
-  |> List.rev
 
 let get_connected board player y x = (* check cell have connection *)
   let ps = [
@@ -30,14 +28,14 @@ let get_connected board player y x = (* check cell have connection *)
 let path_equal path path' = (* compare list of coordinate *)
   List.map2 path path' ~f:(fun (y1, x1) (y2, x2) -> y1 = y2 && x1 = x2)
   |> function
-    | Ok l -> not (List.exists l ~f:(fun e -> Bool.equal e false))
+    | Ok l -> List.for_all l ~f:(Bool.equal true)
     | Unequal_lengths -> false
 
 let add_path board player stones = (* get connect coordinate for each element *)
   List.fold stones ~init:stones ~f:(fun acc (y, x) ->
     List.append acc (get_connected board player y x)) (* append connected *)
   |> List.dedup_and_sort ~compare:(fun (y1, x1) (y2, x2) ->
-      if Int.(y1 <> y2) then y1 - y2 else x1 - x2) (* remove duplicate *)
+      if y1 <> y2 then y1 - y2 else x1 - x2)
 
 let rec get_paths board player width stones =
   let stones' = add_path board player stones in
@@ -51,16 +49,15 @@ let compute board player =
   let start_ps = get_ps board player 0 in (* get first row *)
   let end_ps = get_ps board player width in (* get last row *)
   let paths = get_paths board player width start_ps in (* get list of coord *)
-  List.map end_ps ~f:(fun (y1, x1) -> (* check if end_ps is present in paths *)
+  List.map end_ps ~f:(fun (y1, x1) ->
     List.exists paths ~f:(fun (y2, x2) -> y1 = y2 && x1 = x2))
-  |> List.exists ~f:(fun e -> Bool.equal e true)
+  |> List.exists ~f:(Bool.equal true)
 
 (* --- Formating *)
 let formatted line =
   line |> String.filter ~f:(fun c -> not (Char.equal c ' ')) |> String.to_list
 
-let rotate board =
-  board |> List.transpose_exn
+let rotate board = board |> List.transpose_exn
 
 (* --- MAIN *)
 let connect board =
