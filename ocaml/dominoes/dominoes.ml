ced6f77c9d71763487e78c489d75fc150795238b
asrs <asrs.contact@gmail.com>
2023-12-19 00:13:14 +0800

    Ocaml: dominoes v1 OK

--- ocaml/dominoes/dominoes.ml
+++ ocaml/dominoes/dominoes.ml
@@ -2,41 +2,50 @@ open Base
 
 type dominoe = (int * int)
 
+(* TOOL FUNCTION *)
 let rotate (x, y) = (y, x)
 
-let can_connect a b = (snd a = fst b)
+let exclude lst i = List.filteri lst ~f:(fun i' _ -> i <> i')
 
-let wavefront_arbiter a b = Int.compare (fst a + snd a) (fst b + snd b)
+let can_connect a b = Int.equal (snd a) (fst b)
 
-let is_dominoe_in lst a = List.exists lst ~f:(fun e -> Poly.equal a e)
+let sort_dominoes lst = List.sort lst ~compare:Poly.compare
 
-let check lst = Int.equal (List.hd_exn lst |> fst) (List.last_exn lst |> snd)
+(* CHECK FUNCTION *)
+let check_loop lst =
+  match (List.hd lst, List.last lst) with
+  | Some a, Some b -> Int.equal (fst a) (snd b)
+  | _ -> false
 
+let check_chain lst =
+  List.zip_exn lst (List.tl_exn lst @ [List.hd_exn lst])
+  |> List.map ~f:(fun (a, b) -> can_connect a b)
+  |> List.for_all ~f:(Bool.equal true)
+
+(* ALGO FUNCTION *)
 let find_connect hd lst =
   List.find lst ~f:(fun e -> can_connect hd e || can_connect hd (rotate e))
   |> function
     | None -> lst
     | Some e -> if can_connect hd e then [e] else [(rotate e)]
 
-let build_chains lst =
-  List.zip_exn lst (List.tl_exn lst @ [List.hd_exn lst])
-  |> List.fold ~init:([],[]) ~f:(fun (acc,rest) (a, b) ->
-  if Poly.equal a b then (a :: acc, a :: rest)
-  else if is_dominoe_in rest a
-  then (acc, rest)
-  else if can_connect a b || can_connect (rotate a) b || can_connect a (rotate b)
-  || can_connect (rotate a) (rotate b)
-  then (acc @ [a], rest)
-  else
-    (acc, a :: rest))
+let rec build_chains acc = function
+  | [] -> acc
+  | lst ->
+      let find = find_connect (List.last_exn acc) lst in
+      let pool = List.filter lst ~f:(fun e -> not (
+          List.exists find ~f:(fun f ->
+            Poly.equal f e || Poly.equal (rotate f) e)))
+      in
+      build_chains (acc @ find) pool
 
-
-
-
+(* MAIN *)
 let chain = function
   | [] as lst -> Some lst
-  | lst -> let ret = List.sort lst ~compare:wavefront_arbiter |> build_chains lst in
-      match check ret with
-      | true  -> Some ret
-      | false -> None
+  | lst ->
+    let len = List.length lst in
+    let lst = sort_dominoes lst in
+    List.mapi lst ~f:(fun i hd -> build_chains [hd] (exclude lst i))
+    |> List.find ~f:(fun c ->
+      (List.length c = len) && check_loop c && check_chain c)
 
