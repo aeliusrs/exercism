3c32b6ed068caa0b16f8eb06e3dc9295483af740
aeliusrs <asrs.contact+dev@gmail.com>
2023-12-22 12:06:55 +0800

    OCaml: rectangles done ~

--- ocaml/rectangles/rectangles.ml
+++ ocaml/rectangles/rectangles.ml
@@ -1,74 +1,54 @@
 open Base
 
-(* ----------------------- TOOL *)
+(* transform the given array to a list of list of char *)
+let sanitize arr = Array.to_list arr |> List.map ~f:String.to_list
 
+(* exclude element at index i from a list *)
 let exclude lst i = List.filteri lst ~f:(fun i' _ -> i <> i')
 
-let rotate (x, y) = (y, x)
+(* collect all char '+', AKA corners *)
+let collect_corners board =
+  List.concat_mapi board ~f:(fun x row ->
+    List.foldi row ~init:[] ~f:(fun y acc c ->
+      if Char.equal c '+' then acc @ [(x, y)]  else acc))
 
-let can_connect a b = Int.equal (snd a) (fst b)
+(* exclude top_left from the list, and reverse order *)
+let generate_pairs tl i lst =
+  exclude lst i
+  |> List.filter ~f:(fun e -> fst tl < fst e && snd tl < snd e)
+  |> List.rev
+  |> List.map ~f:(fun br -> (tl, br))
 
 (* check that all element in a row are valid *)
 let is_row board row_idx start_idx end_idx =
-  let board_height = List.length board in
-  if row_idx < 0 || row_idx >= board_height then
-    false
-  else
-    board
-    |> Fn.flip List.nth_exn row_idx
-    |> List.sub ~pos:start_idx ~len:(end_idx - start_idx)
-    |> List.for_all ~f:(fun c -> Char.equal c '-' || Char.equal c '+')
+  List.nth board row_idx
+  |> function | None -> false | Some l ->
+     List.sub l ~pos:start_idx ~len:(end_idx - start_idx + 1)
+     |> List.for_all ~f:(fun c -> Char.equal c '-' || Char.equal c '+')
 
 (* check that all element in take sub col are valid *)
 let is_col board col_idx start_idx end_idx =
-  let board_height = List.length board in
-  if start_idx < 0 || start_idx > board_height || end_idx >= board_height then
-    false
-  else
-    board
-    |> List.transpose_exn
-    |> Fn.flip List.nth_exn col_idx
-    |> List.sub ~pos:start_idx ~len:(end_idx - start_idx)
-    |> List.for_all ~f:(fun c -> Char.equal c '|' || Char.equal c '+')
+  board
+  |> List.transpose_exn
+  |> Fn.flip List.nth col_idx
+  |> function | None -> false | Some l ->
+     List.sub l ~pos:start_idx ~len:(end_idx - start_idx + 1)
+     |> List.for_all ~f:(fun c -> Char.equal c '|' || Char.equal c '+')
 
-(* ----------------------- get COORDINATE *)
-let collect_corners board =
-  List.concat_mapi board ~f:(fun x str ->
-    List.foldi str ~init:[] ~f:(fun y acc c ->
-      if Char.equal c '+' then acc @ [(x, y)]  else acc))
-
-let validate_corners board hd lst =
-  List.filter lst ~f:(fun e' ->
-    match hd, e' with
-    | (x, y), (x', y') when x <= x' && y <= y' ->
-        is_row board x y y' || is_col board y x x'
-    | _ -> false)
-
+(* generate top_right and bottom left coord
+   then check the connection between them are valid *)
 let is_rectangle board tl br =
-  let lst = [(snd br, fst tl);(snd tl, fst br)] in
-  let tl_lst = (validate_corners board tl lst) in
-  let br_lst = (validate_corners board br lst) in
-  List.length tl_lst = 2 && List.length br_lst = 2
+  is_row board (fst tl) (snd tl) (snd br) &&
+  is_row board (fst br) (snd tl) (snd br) &&
+  is_col board (snd tl) (fst tl) (fst br) &&
+  is_col board (snd br) (fst tl) (fst br)
 
-(* Collect all '+' knows as corners
-   Sort the list
-   Generate List of sublist for each coordinate of corners
-   apply validate_corners to check the connection are valid character
-   Filter to keep only the coordinate that have 4 or more connection *)
-let get_corners board =
+(* collect, generate needed coordinate and possible match, and apply
+   is_rectangle to get a list of bool, then count number of bool *)
+let get_rectangles board =
   collect_corners board
-  |> fun lst -> List.mapi lst ~f:(fun i top_left ->
-      let lst' = exclude lst i
-        |> List.filter ~f:(fun e -> Poly.compare top_left e <= 0)
-        |> List.rev
-      in
-      (top_left, lst'))
-  |> List.map ~f:(fun (tl, lst) ->
-      tl, List.map lst ~f:(fun br -> is_rectangle board tl br))
+  |> fun lst -> List.concat_mapi lst ~f:(fun i tl -> generate_pairs tl i lst)
+  |> List.map ~f:(fun (tl, br) -> is_rectangle board tl br |> Bool.to_int)
+  |> List.fold ~init:0 ~f:(+)
 
-
-
-(* ----------------------- MAIN *)
-let sanitize arr = Array.to_list arr |> List.map ~f:String.to_list
-
-let count_rectangles board = sanitize board |> get_corners |> compute
+let count_rectangles board = sanitize board |> get_rectangles
