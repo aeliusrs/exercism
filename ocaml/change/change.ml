209859f50fcc07fc396761f91bdd8da08da8ccb4
aeliusrs <asrs.contact+dev@gmail.com>
2023-12-28 17:57:33 +0800

    Ocaml: Change is quite nice, i want it better

--- ocaml/change/change.ml
+++ ocaml/change/change.ml
@@ -1,28 +1,29 @@
 open Base
 
-let pick (s,lst) (s', lst') = if s <= s' then (s, lst) else (s', lst')
+let pick a b = if Poly.compare a b < 0 then a else b
 
 let init_step = function | 0 -> (0, []) | _ -> (Int.max_value, [])
 
-let get i coin step = (* get function *)
+let update i coin step = (* get function *)
   if i >= coin && fst step.(i - coin) <> Int.max_value then
     (1 + fst step.(i - coin), coin :: snd step.(i - coin))
   else (Int.max_value, [])
 
-(* Algo *)
-let find_change target coins =
+let find_coin i coins step = (* fold on coins *)
+  List.fold coins ~init:(Int.max_value, []) ~f:(fun acc coin ->
+    pick (update i coin step) acc)
+
+(*
+   init memoize array
+   init a range from 1 to target, for each, fold on coins and try
+   to find best one
+*)
+let compute_change target coins =
   let step = Array.init (Int.succ target) ~f:init_step in
   let range = List.init target ~f:Int.succ in
-  List.iter range ~f:(fun i ->
-    let v =
-      List.fold coins ~init:(Int.max_value, []) ~f:(fun acc coin -> pick (get i coin step) acc)
-      in
-        step.(i) <- v;);
-   step.(target)
-
-let compute_change t c =
-  match find_change t (List.rev c) with
-  | (i, _) when i = Int.max_value -> Error "can't make target with given coins"
+  List.iter range ~f:(fun i -> step.(i) <- (find_coin i coins step));
+  match step.(target) with
+  | (v, _) when v = Int.max_value -> Error "can't make target with given coins"
   | (_, lst) -> Ok (List.sort lst ~compare:Int.compare)
 
 (* Main *)
