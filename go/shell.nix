c02deb1ca836bff1aef37bba795c651fa4fbdaa0
aeliusrs <asrs.contact+dev@gmail.com>
2024-01-08 10:02:55 +0800

    Go: Hello world

---
+++ go/shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+(pkgs.buildFHSUserEnv {
+  name = "golangzone";
+  profile = ''
+      export PATH=$PATH:${pkgs.go}/bin
+  '';
+  targetPkgs = pkgs: (with pkgs; [
+    libGL
+    glib
+    libcap
+    gcc
+    go
+    go-tools
+  ]);
+  runScript = "bash";
+}).env
+
+# run test with color
+# go test -v . | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
