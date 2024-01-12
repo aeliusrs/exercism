a31b22bee3603846ba34b1b087fcce37b12aadc5
aeliusrs <asrs.contact+dev@gmail.com>
2024-01-12 10:33:44 +0800

    Go: refactor reverse string in shorter version, seems to have worst performance tho'

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,12 +1,8 @@
 package reverse
 
-func Reverse(input string) string {
-	runes := []rune(input)
-	length := len(runes)
-	rev := make([]rune, length)
-
-	for i, j := 0, length-1; i < length; i, j = i+1, j-1 {
-        rev[i] = runes[j]
-    }
-	return string(rev)
+func Reverse(input string) (rev string) {
+	for _, c := range input {
+		rev = string(c) + rev
+	}
+	return
 }
