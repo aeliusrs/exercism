651e70a72f6f555de941dff21a6fab507e425729
aeliusrs <asrs.contact+dev@gmail.com>
2024-01-12 11:35:10 +0800

    Go: Resistor trio refactoring, to use math notation to simplify dividing and labeling

--- go/resistor-color-trio/resistor_color_trio.go
+++ go/resistor-color-trio/resistor_color_trio.go
@@ -5,29 +5,19 @@ import (
 	"fmt"
 )
 
-var Resistor = map[string]int{
+var Res = map[string]int{
 		"black": 0, "brown": 1, "red": 2, "orange": 3,
 		"yellow": 4, "green": 5, "blue": 6, "violet": 7,
 		"grey": 8, "white": 9,
 	}
 
 func Label(colors []string) string {
-	var label string
-	sum := (Resistor[colors[0]] * 10 + Resistor[colors[1]]) *
-		int(math.Pow10(Resistor[colors[2]]))
+	sum := (Res[colors[0]] * 10 + Res[colors[1]]) * int(math.Pow10(Res[colors[2]]))
 
 	switch {
-	  case sum == 0: label = ""
-	  case sum % 1_000_000_000 == 0:
-		sum = sum / 1_000_000_000
-		label = "giga"
-	  case sum % 1_000_000 == 0:
-		sum = sum / 1_000_000
-		label = "mega"
-	  case sum % 1_000 == 0:
-		sum = sum / 1_000
-		label = "kilo"
+		case sum/10e8 > 1: return fmt.Sprintf("%d gigaohms", sum/10e8)
+		case sum/10e5 > 1: return fmt.Sprintf("%d megaohms", sum/10e5)
+		case sum/10e2 > 1: return fmt.Sprintf("%d kiloohms", sum/10e2)
+		default: return fmt.Sprintf("%d ohms", sum)
 	}
-
-	return fmt.Sprintf("%d %sohms", sum, label)
 }
