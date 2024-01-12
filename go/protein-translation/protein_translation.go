55f9f01f9c647805db594787675b7f5781213c2d
aeliusrs <asrs.contact+dev@gmail.com>
2024-01-12 16:57:19 +0800

    Go: protein_translation is done

---
+++ go/protein-translation/protein_translation.go
@@ -0,0 +1,55 @@
+package protein
+
+import "errors"
+
+var (
+	ErrInvalidBase = errors.New("invalid RNA base")
+	ErrStop        = errors.New("stop codon")
+)
+
+var protein = map[string]string{
+	"AUG": "Methionine",
+	"UUU": "Phenylalanine",
+	"UUC": "Phenylalanine",
+	"UUA": "Leucine",
+	"UUG": "Leucine",
+	"UCU": "Serine",
+	"UCC": "Serine",
+	"UCA": "Serine",
+	"UCG": "Serine",
+	"UAU": "Tyrosine",
+	"UAC": "Tyrosine",
+	"UGU": "Cysteine",
+	"UGC": "Cysteine",
+	"UGG": "Tryptophan",
+	"UAA": "STOP",
+	"UAG": "STOP",
+	"UGA": "STOP",
+}
+
+
+func FromRNA(rna string) ([]string, error) {
+	var acc []string
+
+	if len(rna) % 3 != 0 { return acc, ErrInvalidBase }
+
+	for i := 0; i < len(rna); i += 3 {
+		p, err := FromCodon(rna[i:(i + 3)])
+
+		if err == ErrInvalidBase {
+			return acc, err
+		} else if err == ErrStop {
+			break
+		}
+		acc = append(acc, p)
+	}
+	return acc, nil
+}
+
+func FromCodon(codon string) (string, error) {
+		p, ok := protein[codon]
+
+		if !ok || len(codon) % 3 != 0 { return "", ErrInvalidBase }
+		if p == "STOP" { return "", ErrStop }
+		return p, nil
+}
