69f30ff9cb7093310681ca5623bc56498f9638f9
aeliusrs <asrs.contact+dev@gmail.com>
2024-01-16 17:09:02 +0800

    Go: Strain done !

---
+++ go/strain/strain.go
@@ -0,0 +1,26 @@
+package strain
+// You will need typed parameters (aka "Generics") to solve this exercise.
+// They are not part of the Exercism syllabus yet but you can learn about
+// them here: https://go.dev/tour/generics/1
+
+type Gen interface {
+	int | []int | string
+}
+
+func filter[T Gen](input []T, strain func(T) bool, valid bool) []T {
+	if input == nil { return input }
+
+	acc := make([]T, 0, len(input))
+	for _, value := range input {
+		if strain(value) == valid { acc = append(acc, value) }
+	}
+	return acc
+}
+
+func Keep[T Gen](input []T, strain func(T) bool) []T {
+	return filter(input, strain, true)
+}
+
+func Discard[T Gen](input []T, strain func(T) bool) []T {
+	return filter(input, strain, false)
+}
