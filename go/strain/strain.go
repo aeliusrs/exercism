76d61ed0c3f0e05f8590d12548ce5ef594642443
aeliusrs <asrs.contact+dev@gmail.com>
2024-01-18 09:43:22 +0800

    Go: update strain

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -3,24 +3,20 @@ package strain
 // They are not part of the Exercism syllabus yet but you can learn about
 // them here: https://go.dev/tour/generics/1
 
-type Gen interface {
-	int | []int | string
-}
 
-func filter[T Gen](input []T, strain func(T) bool, valid bool) []T {
-	if input == nil { return input }
-
+func filter[T any](input []T, strain func(T) bool, valid bool) []T {
 	acc := make([]T, 0, len(input))
+
 	for _, value := range input {
 		if strain(value) == valid { acc = append(acc, value) }
 	}
 	return acc
 }
 
-func Keep[T Gen](input []T, strain func(T) bool) []T {
+func Keep[T any](input []T, strain func(T) bool) []T {
 	return filter(input, strain, true)
 }
 
-func Discard[T Gen](input []T, strain func(T) bool) []T {
+func Discard[T any](input []T, strain func(T) bool) []T {
 	return filter(input, strain, false)
 }
