3e95ebdf40c4d569d33be1c26245bc96ef2126e2
aeliusrs <asrs.contact+dev@gmail.com>
2024-01-23 15:45:21 +0800

    Go: Refactoring custom_set to make it more performant and readable !

--- go/custom-set/custom_set.go
+++ go/custom-set/custom_set.go
@@ -2,9 +2,9 @@ package stringset
 
 import "strings"
 
-type Set map[string]bool
+type Set map[string]bool //map can not have duplicate
 
-func New() Set { return make(Set) }
+func New() Set { return Set{} }
 
 func NewFromSlice(l []string) (acc Set) {
 	acc = New()
@@ -12,7 +12,7 @@ func NewFromSlice(l []string) (acc Set) {
 }
 
 func (s Set) String() string {
-	var acc strings.Builder
+	var acc strings.Builder //more effective than "+" op on string type
 	var length int = len(s) - 1
 	var i int = 0
 
@@ -32,34 +32,30 @@ func (s Set) String() string {
 
 func (s Set) IsEmpty() bool { return len(s) == 0 }
 
-func (s Set) Has(elem string) bool { _, ok := s[elem]; return ok }
+func (s Set) Has(elem string) bool { return s[elem] }
 
-func (s Set) Add(elem string) { if _, ok := s[elem]; !ok { s[elem] = true } }
+func (s Set) Add(elem string) { s[elem] = true } //if exist remain unchanged
 
 func Subset(s1, s2 Set) bool {
-	for k, _ := range s1 { if !s2.Has(k) { return false } }
+	for k, _ := range s1 { if !s2[k] { return false } }
 	return true
 }
 
 func Disjoint(s1, s2 Set) bool {
-	for k := range s1 { if s2.Has(k) { return false } }
+	for k := range s1 { if s2[k] { return false } }
 	return true
 }
 
 func Equal(s1, s2 Set) bool {
 	if len(s1) != len(s2) { return false }
-
-	for k, _ := range s1 {
-		if !s2.Has(k) { return false }
-	}
-	return true
+	return Subset(s1, s2) && Subset(s2, s1)
 }
 
 func Intersection(s1, s2 Set) Set {
 	result := New()
 
 	for k, _ := range s1 {
-		if s2.Has(k) { result.Add(k) }
+		if s2[k] { result.Add(k) }
 	}
 	return result
 }
@@ -68,7 +64,7 @@ func Difference(s1, s2 Set) Set {
 	result := New()
 
 	for k, _ := range s1 {
-		if !s2.Has(k) { result.Add(k) }
+		if !s2[k] { result.Add(k) }
 	}
 	return result
 }
