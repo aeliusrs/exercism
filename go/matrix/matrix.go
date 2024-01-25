ba83716b693e2fc7a382f67728630f7bdee78a99
asrs <asrs.contact@gmail.com>
2024-01-25 11:29:55 +0800

    Go: Matrix small optimization

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -3,9 +3,7 @@ package matrix
 import "strings"
 import "strconv"
 import "errors"
-//import "unicode"
 
-// Define the Matrix type here.
 type Matrix [][]int
 
 func New(s string) (Matrix, error) {
@@ -13,35 +11,52 @@ func New(s string) (Matrix, error) {
 	
 	acc := make([][]int, len(rows))
 	for i, row := range rows {
-		cols := strings.Split(row, " ")
+		cols := strings.Fields(row)
 
 		intCols := make([]int, len(cols))
 		for j, col := range cols {
-			if col != "" {
-				n, err := strconv.Atoi(col)
+			n, err := strconv.Atoi(col)
+			if err != nil { return nil, err }
 
-				if err != nil { return nil, err }
-				intCols[j] = n
-			}
-		}
-
-		if i > 0 && len(intCols) != len(acc[i - 1]) {
-			return nil, errors.New("can not be empty")
+			intCols[j] = n
 		}
 		acc[i] = intCols
+
+		if i > 0 && len(intCols) != len(acc[0]) {
+			return nil, errors.New("empty or uneven row")
+		}
 	}
 	return acc, nil
 }
 
-// Cols and Rows must return the results without affecting the matrix.
-func (m Matrix) Cols() [][]int {
-	panic("Please implement the Cols function")
+func (m Matrix) Rows() [][]int {
+	acc := make([][]int, len(m))
+
+    for i := range m {
+        acc[i] = make([]int, len(m[i]))
+        copy(acc[i], m[i])
+    }
+	return acc
 }
 
-func (m Matrix) Rows() [][]int {
-	panic("Please implement the Rows function")
+func (m Matrix) Cols() [][]int {
+    acc := make([][]int, len(m[0]))
+
+    for j := range m[0] {
+        col := make([]int, len(m))
+        for i := range m {
+            col[i] = m[i][j]
+        }
+        acc[j] = col
+    }
+    return acc
 }
 
 func (m Matrix) Set(row, col, val int) bool {
-	panic("Please implement the Set function")
+	switch {
+		case row < 0 || row >= len(m):  return false
+		case col < 0 || col >= len(m[0]): return false
+		default: m[row][col] = val
+	}
+	return true
 }
